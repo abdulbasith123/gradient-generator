239a215e719c59282d1c69a9d56080d198381bc9
basithkalam <basithkalam@gmail.com>
2022-12-08 00:34:00 +0530

    removed unwanted array code

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 var _=require('lodash');
 
-
-var array1=[1,2,3,4,5,6,7];
-console.log(_.without(array1,3));
-var array2=[1,2,3,4,5,6,7];
-console.log(_.without(array2,5));
 var css = document.getElementsByTagName("h3")[0];
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
